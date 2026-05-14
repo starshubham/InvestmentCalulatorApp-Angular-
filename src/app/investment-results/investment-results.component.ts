98ea33abadcaf01515b38f3106e966ab5eafb44e
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-05-14 08:11:17 +0530

    Using Signals in Services

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
@@ -32,7 +32,10 @@ export class InvestmentResultsComponent {
 
   private investmentService = inject(InvestmentService);
 
-  get results() {
-    return this.investmentService.resultsData;
-  }
+  // get results() {
+  //   return this.investmentService.resultsData;
+  // }
+
+  results = computed(() => this.investmentService.resultsData());
+  // results = this.investmentService.resultsData.asReadonly();
 }
